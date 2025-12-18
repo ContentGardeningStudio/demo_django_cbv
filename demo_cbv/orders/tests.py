174e81e14bc66c0a3b9b654a48dfd95db1c0965d
djimi <djimi9530@gmail.com>
2025-12-18 16:32:34 +0100

    test order list view

--- demo_cbv/orders/tests.py
+++ demo_cbv/orders/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
+from django import test
+import pytest
 
-# Create your tests here.
+
+@pytest.mark.django_db
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = test.Client()
+
+    def test_order_list_view_status_code(self):
+        url = reverse("order-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
